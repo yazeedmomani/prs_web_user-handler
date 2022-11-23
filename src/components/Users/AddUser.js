0e457b6fac7b3d8f0e85d3b7a44a45bab9e2585b
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-23 23:25:20 +0300

    manage user input state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 function AddUser(props) {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
   const addUserHandler = function (event) {
     event.preventDefault();
+
+    console.log(username, age);
+
+    setUsername("");
+    setAge("");
+  };
+
+  const usernameChangeHandler = function (event) {
+    setUsername(event.target.value);
+  };
+
+  const ageChangeHandler = function (event) {
+    setAge(event.target.value);
   };
 
   return (
@@ -14,11 +31,15 @@ function AddUser(props) {
         <input
           id="username"
           type="text"
+          value={username}
+          onChange={usernameChangeHandler}
         />
         <label htmlFor="age">Age (Years)</label>
         <input
           id="age"
           type="number"
+          value={age}
+          onChange={ageChangeHandler}
         />
         <Button type="submit">Add User</Button>
       </form>
