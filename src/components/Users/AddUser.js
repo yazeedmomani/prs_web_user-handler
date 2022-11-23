ce11778f9f24e10386341d02f4389912eb4b10a7
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-23 23:05:56 +0300

    add Card.js

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,27 @@
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
 function AddUser(props) {
   const addUserHandler = function (event) {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">username</label>
-      <input
-        id="username"
-        type="text"
-      />
-      <label htmlFor="age">Age (Years)</label>
-      <input
-        id="age"
-        type="number"
-      />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">username</label>
+        <input
+          id="username"
+          type="text"
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+        />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 }
 
