df3f2c177a2ede05de71b9ee67c43afe24a243bd
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-24 02:45:39 +0300

    create UsersList

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,18 @@
+import Card from "../UI/Card";
+import classes from "./UsersList.module.css";
+
+function UsersList(props) {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((current) => (
+          <li>
+            {current.name} ({current.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+export default UsersList;
